2d4314074c16d9082dfe127b42d606b68e2706ed
Michael McDermott <michael@mgmcdermott.com>
2016-02-10 13:31:44 -0500

    Updates.

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ $(function() {
     $('.assay-val').html(JSON.stringify($('#assay').bioinput('entities')));
     $('.cell-val').html(JSON.stringify($('#cell').bioinput('entities')));
 
+    $
+      .get('http://amp.pharm.mssm.edu/biocomplete/api/v1/counts')
+      .done(function(data) {
+        $('.cell-line-count').html(JSON.stringify(data.cellLines));
+      });
+
   }).trigger('change');
 });

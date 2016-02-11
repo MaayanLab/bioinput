c399ade43fcf4ead05a0411a07446e1c354990f2
Michael McDermott <michael@mgmcdermott.com>
2016-02-11 13:45:49 -0500

    Fix index.js call to counts.

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 /* eslint-disable */
 $(function() {
   $('input').on('change', function() {
-
     $('.assay-val').html(JSON.stringify($('#assay').bioinput('entities')));
     $('.cell-val').html(JSON.stringify($('#cell').bioinput('entities')));
-
-    $
-      .get('http://amp.pharm.mssm.edu/biocomplete/api/v1/counts')
-      .done(function(data) {
-        $('.assay-count').html(JSON.stringify(data.assays));
-        $('.cell-line-count').html(JSON.stringify(data.cellLines));
-      });
-
   }).trigger('change');
+  $.get('http://amp.pharm.mssm.edu/biocomplete/api/v1/counts')
+    .done(function(data) {
+      $('.assay-count').html(JSON.stringify(data.assays));
+      $('.cell-line-count').html(JSON.stringify(data.cellLines));
+    });
 });

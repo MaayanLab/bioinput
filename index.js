ebadbe5543c11ec7a439054067feccd729e3cab1
Michael McDermott <michael@mgmcdermott.com>
2016-02-11 13:36:30 -0500

    Pushing to v0.2. Added assay counts.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ $(function() {
     $
       .get('http://amp.pharm.mssm.edu/biocomplete/api/v1/counts')
       .done(function(data) {
+        $('.assay-count').html(JSON.stringify(data.assays));
         $('.cell-line-count').html(JSON.stringify(data.cellLines));
       });
 
